3e4f45db3bf6b856ed01658d0b9bc06b7e1c5a5c
Venkata Ravi Kumar Yeluru <rkyeluru@gmail.com>
2026-06-11 17:07:22 -0400

    style: set default theme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       <head>
         <script dangerouslySetInnerHTML={{ __html: `
           (function() {
-            var theme = localStorage.getItem('theme') || 'light';
+            var theme = localStorage.getItem('theme') || 'dark';
             document.documentElement.setAttribute('data-theme', theme);
           })();
         ` }} />
